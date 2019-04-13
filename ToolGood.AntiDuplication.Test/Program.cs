f3bae2b3eda6786b8c37521e4f0631bfb5deeb5a
toolgood <toolgood@qq.com>
2019-04-13 11:14:03 +0800

    修改 README.md

--- ToolGood.AntiDuplication.Test/Program.cs
+++ ToolGood.AntiDuplication.Test/Program.cs
@@ -11,20 +11,25 @@ namespace ToolGood.AntiDuplication.Test
 {
     class Program
     {
-        private static AntiDupCache<int, int> antiDupCache = new AntiDupCache<int, int>(50, 1);
-        private static AntiDupQueue<int, int> antiDupQueue = new AntiDupQueue<int, int>(50);
-        private static DictCache<int, int> dictCache = new DictCache<int, int>();
-        private static Cache<int, int> cache = new Cache<int, int>();
+        private readonly static AntiDupCache<int, int> antiDupCache = new AntiDupCache<int, int>(50, 1);
+        private readonly static AntiDupQueue<int, int> antiDupQueue = new AntiDupQueue<int, int>(50);
+        private readonly static DictCache<int, int> dictCache = new DictCache<int, int>();
+        private readonly static Cache<int, int> cache = new Cache<int, int>();
 
 
         static void Main(string[] args)
         {
             var processorCount = Environment.ProcessorCount;
-            for (int slim = 1; slim <= processorCount; slim++) {
-                for (int count = 1; count <= processorCount; count++) {
-                    Test(count, slim);
-                }
+            Test2(10000, processorCount);
+
+            Test3(1000, processorCount);
+
+            for (int count = 1; count <= processorCount; count++) {
+                Test(count, processorCount);
             }
+
+
+
 
             Console.WriteLine("----------------------- 结束 -----------------------");
             Console.ReadLine();
@@ -33,76 +38,302 @@ namespace ToolGood.AntiDuplication.Test
 
 
 
-        private static void Test(int count, int lism = 8)
+        private static void Test(int count, int lism)
         {
             var list = Build(count);
             antiDupCache.Clear();
             antiDupQueue.Clear();
             dictCache.Clear();
             cache.Clear();
-            Console.WriteLine($"----------------------- 开始  从1到100   重复次数：{count}  并发数：{lism} -----------------------");
+            Console.WriteLine($"----------------------- 开始  从1到100   重复次数：{count} 单位： ms -----------------------");
+            Console.Write("      并发数量： ");
+            for (int i = 1; i <= lism; i++) {
+                Console.Write(i.ToString().PadRight(5));
+            }
+            Console.Write("\r\n");
 
             var stopwatch = Stopwatch.StartNew();
-            Parallel.ForEach(list, new ParallelOptions() { MaxDegreeOfParallelism = lism }, (j) => {
-                Thread.Sleep(1);
-            });
-            stopwatch.Stop();
-            Console.WriteLine("使用普通并发：" + stopwatch.ElapsedMilliseconds + "ms");
-
-            stopwatch = Stopwatch.StartNew();
-            Parallel.ForEach(list, new ParallelOptions() { MaxDegreeOfParallelism = lism }, (j) => {
-                antiDupCache.Execute(j, () => {
+            Console.Write("      普通并发：");
+            for (int i = 1; i <= lism; i++) {
+                stopwatch = Stopwatch.StartNew();
+                Parallel.ForEach(list, new ParallelOptions() { MaxDegreeOfParallelism = i }, (j) => {
                     Thread.Sleep(1);
-                    return j;
                 });
-            });
-            stopwatch.Stop();
-            Console.WriteLine("使用AntiDupCache：" + stopwatch.ElapsedMilliseconds + "ms");
+                stopwatch.Stop();
+                Console.Write(" " + stopwatch.ElapsedMilliseconds.ToString().PadRight(4));
+            }
+            Console.Write("\r\n");
+
+
+            Console.Write("  AntiDupCache：");
+            for (int i = 1; i <= lism; i++) {
+                antiDupCache.Clear();
+                stopwatch = Stopwatch.StartNew();
+                Parallel.ForEach(list, new ParallelOptions() { MaxDegreeOfParallelism = i }, (j) => {
+                    antiDupCache.Execute(j, () => {
+                        Thread.Sleep(1);
+                        return j;
+                    });
+                });
+                stopwatch.Stop();
+                Console.Write(" " + stopwatch.ElapsedMilliseconds.ToString().PadRight(4));
+            }
+            Console.Write("\r\n");
+
+            Console.Write("  AntiDupQueue：");
+            for (int i = 1; i <= lism; i++) {
+                antiDupQueue.Clear();
+                stopwatch = Stopwatch.StartNew();
+                Parallel.ForEach(list, new ParallelOptions() { MaxDegreeOfParallelism = i }, (j) => {
+                    antiDupQueue.Execute(j, () => {
+                        Thread.Sleep(1);
+                        return j;
+                    });
+                });
+                stopwatch.Stop();
+                Console.Write(" " + stopwatch.ElapsedMilliseconds.ToString().PadRight(4));
+            }
+            Console.Write("\r\n");
+
+            Console.Write("     DictCache：");
+            for (int i = 1; i <= lism; i++) {
+                dictCache.Clear();
+                stopwatch = Stopwatch.StartNew();
+                Parallel.ForEach(list, new ParallelOptions() { MaxDegreeOfParallelism = i }, (j) => {
+                    dictCache.Execute(j, () => {
+                        Thread.Sleep(1);
+                        return j;
+                    });
+                });
+                stopwatch.Stop();
+                Console.Write(" " + stopwatch.ElapsedMilliseconds.ToString().PadRight(4));
+            }
+            Console.Write("\r\n");
+
+            Console.Write("         Cache：");
+            for (int i = 1; i <= lism; i++) {
+                cache.Clear();
+                stopwatch = Stopwatch.StartNew();
+                Parallel.ForEach(list, new ParallelOptions() { MaxDegreeOfParallelism = i }, (j) => {
+                    cache.Execute(j, () => {
+                        Thread.Sleep(1);
+                        return j;
+                    });
+                });
+                stopwatch.Stop();
+                Console.Write(" " + stopwatch.ElapsedMilliseconds.ToString().PadRight(4));
+            }
+            Console.Write("\r\n");
+
 
             stopwatch = Stopwatch.StartNew();
-            Parallel.ForEach(list, new ParallelOptions() { MaxDegreeOfParallelism = lism }, (j) => {
-                antiDupQueue.Execute(j, () => {
+            Console.Write("第二次普通并发：");
+            for (int i = 1; i <= lism; i++) {
+                stopwatch = Stopwatch.StartNew();
+                Parallel.ForEach(list, new ParallelOptions() { MaxDegreeOfParallelism = i }, (j) => {
                     Thread.Sleep(1);
-                    return j;
                 });
-            });
-            stopwatch.Stop();
-            Console.WriteLine("使用AntiDupQueue：" + stopwatch.ElapsedMilliseconds + "ms");
+                stopwatch.Stop();
+                Console.Write(" " + stopwatch.ElapsedMilliseconds.ToString().PadRight(4));
+            }
+            Console.Write("\r\n");
+            Console.Write("\r\n");
 
-            stopwatch = Stopwatch.StartNew();
-            Parallel.ForEach(list, new ParallelOptions() { MaxDegreeOfParallelism = lism }, (j) => {
-                dictCache.Execute(j, () => {
-                    Thread.Sleep(1);
-                    return j;
-                });
-            });
-            stopwatch.Stop();
-            Console.WriteLine("使用DictCache：" + stopwatch.ElapsedMilliseconds + "ms");
-
-
-            stopwatch = Stopwatch.StartNew();
-            Parallel.ForEach(list, new ParallelOptions() { MaxDegreeOfParallelism = lism }, (j) => {
-                cache.Execute(j, () => {
-                    Thread.Sleep(1);
-                    return j;
-                });
-            });
-            stopwatch.Stop();
-            Console.WriteLine("使用Cache：" + stopwatch.ElapsedMilliseconds + "ms");
-
-            stopwatch = Stopwatch.StartNew();
-            Parallel.ForEach(list, new ParallelOptions() { MaxDegreeOfParallelism = lism }, (j) => {
-                Thread.Sleep(1);
-            });
-            stopwatch.Stop();
-            Console.WriteLine("第二次使用普通并发：" + stopwatch.ElapsedMilliseconds + "ms");
         }
+
+        private static void Test2(int count, int lism)
+        {
+            var list = Build(count);
+            Console.WriteLine($"----------------------- 测试缓存性能 从1到100 重复次数：{count} 单位： ms -----------------------");
+            Console.Write("    并发数量： ");
+            for (int i = 1; i <= lism; i++) {
+                Console.Write(i.ToString().PadRight(5));
+            }
+            Console.Write("\r\n");
+
+            var stopwatch = Stopwatch.StartNew();
+            Console.Write("AntiDupCache：");
+            for (int i = 1; i <= lism; i++) {
+                antiDupCache.Clear();
+                stopwatch = Stopwatch.StartNew();
+                Parallel.ForEach(list, new ParallelOptions() { MaxDegreeOfParallelism = i }, (j) => {
+                    antiDupCache.Execute(j, () => {
+                        return j;
+                    });
+                });
+                stopwatch.Stop();
+                Console.Write(" " + stopwatch.ElapsedMilliseconds.ToString().PadRight(4));
+            }
+            Console.Write("\r\n");
+
+            Console.Write("AntiDupQueue：");
+            for (int i = 1; i <= lism; i++) {
+                antiDupQueue.Clear();
+                stopwatch = Stopwatch.StartNew();
+                Parallel.ForEach(list, new ParallelOptions() { MaxDegreeOfParallelism = i }, (j) => {
+                    antiDupQueue.Execute(j, () => {
+                        return j;
+                    });
+                });
+                stopwatch.Stop();
+                Console.Write(" " + stopwatch.ElapsedMilliseconds.ToString().PadRight(4));
+            }
+            Console.Write("\r\n");
+
+            Console.Write("   DictCache：");
+            for (int i = 1; i <= lism; i++) {
+                dictCache.Clear();
+                stopwatch = Stopwatch.StartNew();
+                Parallel.ForEach(list, new ParallelOptions() { MaxDegreeOfParallelism = i }, (j) => {
+                    dictCache.Execute(j, () => {
+                        return j;
+                    });
+                });
+                stopwatch.Stop();
+                Console.Write(" " + stopwatch.ElapsedMilliseconds.ToString().PadRight(4));
+            }
+            Console.Write("\r\n");
+
+            Console.Write("       Cache：");
+            for (int i = 1; i <= lism; i++) {
+                cache.Clear();
+                stopwatch = Stopwatch.StartNew();
+                Parallel.ForEach(list, new ParallelOptions() { MaxDegreeOfParallelism = i }, (j) => {
+                    cache.Execute(j, () => {
+                        return j;
+                    });
+                });
+                stopwatch.Stop();
+                Console.Write(" " + stopwatch.ElapsedMilliseconds.ToString().PadRight(4));
+            }
+            Console.Write("\r\n");
+            Console.Write("\r\n");
+
+        }
+
+        private static void Test3(int count, int lism)
+        {
+            var list = Build2(count);
+            antiDupCache.Clear();
+            antiDupQueue.Clear();
+            dictCache.Clear();
+            cache.Clear();
+            Console.WriteLine($"----------------------- 防线上环境  从1到{count}  单位： ms -----------------------");
+            Console.Write("      并发数量： ");
+            for (int i = 1; i <= lism; i++) {
+                Console.Write(i.ToString().PadRight(5));
+            }
+            Console.Write("\r\n");
+
+            var stopwatch = Stopwatch.StartNew();
+            Console.Write("      普通并发：");
+            for (int i = 1; i <= lism; i++) {
+                stopwatch = Stopwatch.StartNew();
+                Parallel.ForEach(list, new ParallelOptions() { MaxDegreeOfParallelism = i }, (j) => {
+                    Thread.Sleep(1);
+                });
+                stopwatch.Stop();
+                Console.Write(" " + stopwatch.ElapsedMilliseconds.ToString().PadRight(4));
+            }
+            Console.Write("\r\n");
+
+
+            Console.Write("  AntiDupCache：");
+            for (int i = 1; i <= lism; i++) {
+                antiDupCache.Clear();
+                stopwatch = Stopwatch.StartNew();
+                Parallel.ForEach(list, new ParallelOptions() { MaxDegreeOfParallelism = i }, (j) => {
+                    antiDupCache.Execute(j, () => {
+                        Thread.Sleep(1);
+                        return j;
+                    });
+                });
+                stopwatch.Stop();
+                Console.Write(" " + stopwatch.ElapsedMilliseconds.ToString().PadRight(4));
+            }
+            Console.Write("\r\n");
+
+            Console.Write("  AntiDupQueue：");
+            for (int i = 1; i <= lism; i++) {
+                antiDupQueue.Clear();
+                stopwatch = Stopwatch.StartNew();
+                Parallel.ForEach(list, new ParallelOptions() { MaxDegreeOfParallelism = i }, (j) => {
+                    antiDupQueue.Execute(j, () => {
+                        Thread.Sleep(1);
+                        return j;
+                    });
+                });
+                stopwatch.Stop();
+                Console.Write(" " + stopwatch.ElapsedMilliseconds.ToString().PadRight(4));
+            }
+            Console.Write("\r\n");
+
+            Console.Write("     DictCache：");
+            for (int i = 1; i <= lism; i++) {
+                dictCache.Clear();
+                stopwatch = Stopwatch.StartNew();
+                Parallel.ForEach(list, new ParallelOptions() { MaxDegreeOfParallelism = i }, (j) => {
+                    dictCache.Execute(j, () => {
+                        Thread.Sleep(1);
+                        return j;
+                    });
+                });
+                stopwatch.Stop();
+                Console.Write(" " + stopwatch.ElapsedMilliseconds.ToString().PadRight(4));
+            }
+            Console.Write("\r\n");
+
+            Console.Write("         Cache：");
+            for (int i = 1; i <= lism; i++) {
+                cache.Clear();
+                stopwatch = Stopwatch.StartNew();
+                Parallel.ForEach(list, new ParallelOptions() { MaxDegreeOfParallelism = i }, (j) => {
+                    cache.Execute(j, () => {
+                        Thread.Sleep(1);
+                        return j;
+                    });
+                });
+                stopwatch.Stop();
+                Console.Write(" " + stopwatch.ElapsedMilliseconds.ToString().PadRight(4));
+            }
+            Console.Write("\r\n");
+
+
+            stopwatch = Stopwatch.StartNew();
+            Console.Write("第二次普通并发：");
+            for (int i = 1; i <= lism; i++) {
+                stopwatch = Stopwatch.StartNew();
+                Parallel.ForEach(list, new ParallelOptions() { MaxDegreeOfParallelism = i }, (j) => {
+                    Thread.Sleep(1);
+                });
+                stopwatch.Stop();
+                Console.Write(" " + stopwatch.ElapsedMilliseconds.ToString().PadRight(4));
+            }
+            Console.Write("\r\n");
+            Console.Write("\r\n");
+
+        }
+
 
         private static List<int> Build(int count)
         {
             List<int> list = new List<int>();
             for (int i = 0; i < 100; i++) {
                 for (int j = 0; j < count; j++) {
+                    list.Add(i);
+                }
+            }
+            return list;
+        }
+
+        private static List<int> Build2(int count)
+        {
+            Random random = new Random();
+
+            List<int> list = new List<int>();
+            for (int i = 0; i < count; i++) {
+                list.Add(i);
+                if (random.NextDouble() > 0.99) {
                     list.Add(i);
                 }
             }
